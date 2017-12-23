7534fa075eb926c5ae1160709bbe5744c9a7fd64
정지원 <4dakto@gmail.com>
2017-12-23 13:50:39 +0900

    Queries with Components

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Lyrical-GraphQL
-Starter project from a GraphQL course on Udemy.com
+# GraphQl + React Strategy
+1. Identify data required
+2. Write query i Graphiql and in component file
+3. Bond query + component
+4. Access data!
--- client/index.js
+++ client/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import AppolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 
+import SongList from './components/SongList'
+
 const client = new AppolloClient({})
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <SongList />
     </ApolloProvider>
   )
 };
