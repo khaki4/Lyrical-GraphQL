9b4adc2b7dab48153d1301b176a3b72cc3a5971f
정지원 <4dakto@gmail.com>
2017-12-23 15:07:43 +0900

    Add React Router

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,12 +3,22 @@ import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
 class SongList extends Component {
+  renderSongs() {
+    console.log(this.props.data)
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.title} className="collection-item">
+          {song.title}
+        </li>
+      )
+    })
+  }
   render() {
-    console.log(this.props)
+    if (this.props.data.loading) return <div>Loading...</div>
     return (
-      <div>
-        SongList
-      </div>
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul>
     )
   }
 }
--- client/index.js
+++ client/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute }from 'react-router'
 import AppolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 
+import App from './components/App'
 import SongList from './components/SongList'
 
 const client = new AppolloClient({})
@@ -10,7 +12,11 @@ const client = new AppolloClient({})
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 };
