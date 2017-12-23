7da27b67d83827073165d8448de8ac8531525219
정지원 <4dakto@gmail.com>
2017-12-23 23:04:52 +0900

    refetching queries

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
+import { Link, hashHistory } from 'react-router'
+import  query from '../queries/fetchSongs'
 
 class SongCreate extends Component {
   constructor(props) {
@@ -16,13 +18,15 @@ class SongCreate extends Component {
     this.props.mutate({
       variables: {
         title: this.state.title,
-      }
-    })
+      },
+      refetchQueries: [{ query }]
+    }).then(() => hashHistory.push('/'))
   }
   
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
