577dc19a23e6a949cc645539674c9c0f453a0344
정지원 <4dakto@gmail.com>
2017-12-23 20:42:40 +0900

    Mutate with params

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class SongCreate extends Component {
   constructor(props) {
@@ -7,11 +9,22 @@ class SongCreate extends Component {
       title: ''
     }
   }
+  
+  onSubmit(e) {
+    e.preventDefault()
+    
+    this.props.mutate({
+      variables: {
+        title: this.state.title,
+      }
+    })
+  }
+  
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
           <input
             onChange={event =>
@@ -25,4 +38,12 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate
+const mutation = gql`
+  mutation AddSong($title: String){
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate)
