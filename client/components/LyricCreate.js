1e0fb7e3e1b5e9bfc82c4855b5da5742c4a9c9db
정지원 <4dakto@gmail.com>
2018-01-01 20:57:33 +0900

    Jiwon ::  add lyric create component

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class LyricCreate extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Add a Lyric</label>
+          <input />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LyricCreate
