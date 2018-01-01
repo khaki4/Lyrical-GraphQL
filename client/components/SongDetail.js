71538774d83dfd9935eeec4797fff26afd947e4c
정지원 <4dakto@gmail.com>
2018-01-01 21:38:01 +0900

    Jiwon ::  enhancing queries

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router'
 import { graphql } from 'react-apollo'
 import fetchSong from '../queries/fetchSong';
 import LyricCreate from './LyricCreate'
+import LyricList from './LyricList'
 
 class SongDetail extends Component {
   render() {
@@ -12,6 +13,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
         <LyricCreate songId={this.props.params.id} />
       </div>
     )
